9f36818051b316e51d21678aad40321436de8d80
sergio <sergioma76@hotmail.com>
2021-04-21 16:04:31 -0600

    se modifico el menu de hamburguesa, ya funciona bn

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,11 @@ const botonMenu= document.querySelector('.panel-btn'),
 presionar= document.querySelector('#menu, a');
 
 botonMenu.addEventListener("click", function(e){
-	document.getElementById("menu").classList.add('active');
-	
+	document.getElementById("menu").classList.toggle('active');
+
 e.preventDefault();
+
+
 
 presionar.addEventListener("click", function(){
 	document.getElementById("menu").classList.remove('active');
@@ -13,4 +15,3 @@ presionar.addEventListener("click", function(){
 
 })
 
-/*si baja pero solo con inicio y no me deja seleccionar todos los menu a ni con querySelectorAll, el css .active esta en header,css linea 104 */
