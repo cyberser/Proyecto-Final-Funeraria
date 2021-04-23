607ff316f79e1d2f3e823038c9d2bf6a0526f33a
sergio <sergioma76@hotmail.com>
2021-04-23 14:32:24 -0600

    se agrego el mensaje de confirmacion del formulario

--- css/validacion.js
+++
@@ -1,8 +0,0 @@
-const d= document;
-window.addEventListener('load', (event) => {
-function validaciones{
-	const $form=d.querySelector("#formulario"),
-	$inputs=d.querySelectorAll("#formulario[required]");
-	console.log($inputs);
-}
-});
