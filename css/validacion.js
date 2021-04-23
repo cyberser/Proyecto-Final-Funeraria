5994997a5e73cc0b506d371b246649c66cd98a79
sergio <sergioma76@hotmail.com>
2021-04-23 13:29:35 -0600

    se agrego el archivo validacio.js

---
+++ css/validacion.js
@@ -0,0 +1,8 @@
+const d= document;
+window.addEventListener('load', (event) => {
+function validaciones{
+	const $form=d.querySelector("#formulario"),
+	$inputs=d.querySelectorAll("#formulario[required]");
+	console.log($inputs);
+}
+});
